968348c5221aaaa71be3cde20a68e458c1d227f9
Sylvain <sylvain.germe@gmail.com>
2015-03-29 00:00:42 +0100

    Project Working with Wikitube, trying then input type camera

---
+++ hackjcvd - copie/platforms/ios/www/plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js
@@ -0,0 +1,224 @@
+cordova.define("com.wikitude.phonegap.WikitudePlugin.WikitudePlugin", function(require, exports, module) { 	
+	/**
+	 * Release date: 09.03.15
+	 */
+
+	var WikitudePlugin = function() {
+
+		/**
+		 *  This is the SDK Key, provided to you after you purchased the Wikitude SDK from http =//www.wikitude.com/store/.
+		 *  If you're having a trial version, leave this string empty.
+		 */
+		//this._sdkKey = "Pt5hdrH3JIpkqquhy1SoSClsP248FOjgLSeE2J7i65xQkHHRc8aKG2qP01QuiwYTfkbCuhhCA4I1NF+AeE5wyupa1VmUNoEH5YQ4lfYHUBnrD51SY9n6QljlY9WjFwjQrIGNDFhNkaVowQnx7xCMcuRyWOs1bDQcfgBzseaR4TpTYWx0ZWRfX/W9Y/L2mV9EcgGXBcj0tVX+v0pQ7YNl+dC9uB/Pm5eILXcW0Izgrc+tdUm36ftvfs5Qaw9JtGXTlOYhSwUupblZYxrNqNFsKIw0knq/d5dpbFWKXqNhvg0E4rbjgNahn5e8+DMEM90w/Dx8adPpPNBsdo1NhF2ljWxueksqYdu/LY/clqjmcZJdW9ZPb6AmWH0KU3xsspTFARKvN0fCvkR/BCiKIEg+DwPUYuALcbywx/zdBKVqGn9NySarpgglCiBDRUtuzzJ1Gh2fxQUq5bGeYqt1++rPxJlJC5mL9Tjghw83rBi7e8VQl91ZPjBxI2EHEMFazPPr8ZNB6QCLmaayPgO2VW9tfBuGuPOJ3e3B+pEAmklbvqT3fH1FZRhXIZQEKWgSHSfXSh8iehrfMkSKHUplkz0HsSyHKct8rkT2DRLXJC43FqfsjDPjEbW5ComaReYZWxSWsM/LUTRP7gXHYzWoC1bl2vXVADoG7EztUyxLrx7pi0FSaSRSXRVczUsXwzZCbTuxvkRdPLxdy1FHeV8Du5YfLO6sadwOTRFkXZSrDCOJ/SvdYhQrvmVPs/+BcLKvjN7//1ftDyXUm37qXlzhiAtr1xZMqrQ4T1LWzexWe2XVPfrPK/COdpPsJpTm5Wsf8PxlyBbZsnVe45UQLyUh8lXylT979w1Q+fHnUUsxvVfXVlQs0YH8A4ksJZOA0qObNkaOeLSsW4LNkuGodjSl3cS18rjHhbL4/RYYJUrRUYKaMYippuuEltTRMzhrGI1v2x03";
+
+		this._sdkKey = "jEXvU/eTlX3N09H7Pn/lH399zM/QUPrbqI+Q/C1WuPgUHD16INIdi8fGMh1jzWek+CWeuL5WLj2Q9PhmevRa21mx/EQ5G0MIKXUAKYoREYSLslmXj8LI5rDdpS0Z63qpsus6aFsgrWy8XNgwqEotiff0zYx0MFeCEfh5MMT9z+FTYWx0ZWRfXyJ90avJRZ/0x9u0mwkqg38ttOcccjQxT/Fljw7rf2idWGQsxJIRpP8J7QGTtjHDn8zBErOVJ7pCxSmOJlaQqdOJr/wOvnJrbTNFh1Y6VJRPEN41Ot34A2TFo6ooPyTofWQlWyBdMvTvEx4FzfcQ1KaWe1dpSIVbsIy0jffSCe6z/UPVX9Kokyiz1EWvhUFhelaSv0ciDDooPB+XiVhb/+jSmwbBDXcRxCtgNXd4JrEL2nQN5/zwisV3USkYGaTCewaDsecO61sGqWwYOUvWkNvc8pXaac/SBKuAUSXTHuiVDjwsl8ZxDgg8ZpegEaGbIbhSQV3bkTLeGkb/O58cOigXhknyomU8Y1kl6KA6JndtLFZHyZXvbxbRGPE2+n55MozjWGtcU6wAkT0fJbvs3+LGGv+3iVGys9Nw4o8y1mtjbdhXHVpTCQkJadCZfCYCxRIHkYiluG81vBujsCqc2K/yJEjy5k4NRxSS2A0wSWn1EgshCyUNfqw="
+		/**
+		 *  The Wikitude SDK can run in different modes.
+		 *      * Geo means, that objects are placed at latitude/longitude positions.
+		 *      * IR means that only image recognition is used in the ARchitect World.
+		 *  When your ARchitect World uses both, geo and ir, than set this value to "IrAndGeo". Otherwise, if the ARchitectWorld only needs image recognition, placing "IR" will require less features from the device and therefore, support a wider range of devices. Keep in mind that image recognition requires a dual core cpu to work satisfyingly.
+		 */
+        this.FeatureGeo         = "geo";
+        this.Feature2DTracking  = "2d_tracking";
+
+        /**
+         *  Start-up configuration: camera position (front or back).
+         */
+        this.CameraPositionUndefined = 0;
+        this.CameraPositionFront     = 1;
+        this.CameraPositionBack      = 2;
+                       
+        /**
+         *  Start-up configuration: camera focus range restriction (for iOS only).
+         */
+        this.CameraFocusRangeNone = 0;
+        this.CameraFocusRangeNear = 1;
+        this.CameraFocusRangeFar  = 2;
+        
+	};
+
+
+	/*
+	 *	=============================================================================================================================
+	 *
+	 *	PUBLIC API
+	 *
+	 *	=============================================================================================================================
+	 */
+
+	/* Managing ARchitect world loading */
+
+	/**
+	 *  Use this function to check if the current device is capable of running ARchitect Worlds.
+	 *
+	 * @param {function} successCallback A callback which is called if the device is capable of running ARchitect Worlds.
+	 * @param {function} errorCallback A callback which is called if the device is not capable of running ARchitect Worlds.
+	 */
+	WikitudePlugin.prototype.isDeviceSupported = function(successCallback, errorCallback, requiredFeatures) {
+
+		// Check if the current device is capable of running Architect Worlds
+		cordova.exec(successCallback, errorCallback, "WikitudePlugin", "isDeviceSupported", [requiredFeatures]);
+	};
+
+	/**
+	 *	Use this function to load an ARchitect World.
+	 *
+     *  @param {function(loadedURL)}  		successCallback		function which is called after a successful launch of the AR world.
+     *  @param {function(error)}		 	errorCallback		function which is called after a failed launch of the AR world.
+     *	@param {String} 					architectWorldPath	The path to a local ARchitect world or to a ARchitect world on a server or your 
+	 *  @param {String} 					worldPath			path to an ARchitect world, either on the device or on e.g. your Dropbox.
+     *  @param {Array} 						requiredFeatures	augmented reality features: a flags mask for enabling/disablibg 
+     *                                  geographic location-based (WikitudePlugin.FeatureGeo) or image recognition-based (WikitudePlugin.Feature2DTracking) tracking.
+	 *  @param {json object} (optional) startupConfiguration	represents the start-up configuration which may look like the following:
+	 *									{
+	 *                               		"cameraPosition": app.WikitudePlugin.CameraPositionBack,
+	 *                                  	    	"*OptionalPlatform*": {
+	 *											"*optionalPlatformKey*": "*optionalPlatformValue*"
+	 *                                      	}
+	 *                               	}
+	 */	 
+	WikitudePlugin.prototype.loadARchitectWorld = function(successCallback, errorCallback, architectWorldPath, requiredFeatures, startupConfiguration) {
+        
+		cordova.exec(successCallback, errorCallback, "WikitudePlugin", "open", [{
+				"SDKKey": this._sdkKey,
+				"ARchitectWorldURL": architectWorldPath,
+				"RequiredFeatures": requiredFeatures,
+		    	"StartupConfiguration" : startupConfiguration
+			}]);
+		
+		// We add an event listener on the resume and pause event of the application lifecycle
+		document.addEventListener("resume", this.onResume, false);
+		document.addEventListener("pause", this.onPause, false);
+	};
+
+	/* Managing the Wikitude SDK Lifecycle */
+	/**
+	 *	Use this function to stop the Wikitude SDK and to remove it from the screen.
+	 */
+	WikitudePlugin.prototype.close = function() {
+
+		document.removeEventListener("pause", this.onPause, false);
+		document.removeEventListener("resume", this.onResume, false);
+
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "close", [""]);
+	};
+
+	/**
+	 *	Use this function to only hide the Wikitude SDK. All location and rendering updates are still active.
+	 */
+	WikitudePlugin.prototype.hide = function() {
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "hide", [""]);
+	};
+
+	/**
+	 *	Use this function to show the Wikitude SDK again if it was hidden before.
+	 */
+	WikitudePlugin.prototype.show = function() {
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "show", [""]);
+	};
+
+	/* Interacting with the Wikitude SDK */
+
+	/**
+	 *	Use this function to call javascript which will be executed in the context of the currently loaded ARchitect World.
+	 *
+	 * @param js The JavaScript that should be evaluated in the ARchitect View.
+	 */
+	WikitudePlugin.prototype.callJavaScript = function(js) {
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "callJavascript", [js]);
+	};
+
+	/**
+	 *	Use this function to set a callback which will be invoked when the ARchitect World opens an architectsdk =// url.
+	 *	document.location = "architectsdk =//opendetailpage?id=9";
+	 *
+	 *	@param onUrlInvokeCallback A function which will be called when the ARchitect World invokes a call to "document.location = architectsdk =//"
+	 */
+	WikitudePlugin.prototype.setOnUrlInvokeCallback = function(onUrlInvokeCallback) {
+		cordova.exec(onUrlInvokeCallback, this.onWikitudeError, "WikitudePlugin", "onUrlInvoke", [""]);
+	};
+
+	/**
+	 *  Use this function to inject a location into the Wikituce SDK.
+	 *
+	 *  @param latitude The latitude which should be simulated
+	 *  @param longitude The longitude which should be simulated
+	 *  @param altitude The altitude which should be simulated
+	 *  @param accuracy The simulated location accuracy
+	 */
+	WikitudePlugin.prototype.setLocation = function(latitude, longitude, altitude, accuracy) {
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "setLocation", [latitude, longitude, altitude, accuracy]);
+	};
+
+	/**
+	 *  Use this function to generate a screenshot from the current Wikitude SDK view.
+	 *
+     *  @param {function(ur)}  successCallback  function which is called after the screen capturing succeeded.
+     *  @param {function(err)} errorCallback    function which is called after capturing the screen has failed.
+	 *  @param includeWebView Indicates if the ARchitect web view should be included in the generated screenshot or not.
+	 *  @param imagePathInBundleorNullForPhotoLibrary If a file path or file name is given, the generated screenshot will be saved in the application bundle. Passing null will save the photo in the device photo library.
+	 */
+	WikitudePlugin.prototype.captureScreen = function(successCallback, errorCallback, includeWebView, imagePathInBundleOrNullForPhotoLibrary)
+    {
+		cordova.exec(successCallback, errorCallback, "WikitudePlugin", "captureScreen", [includeWebView, imagePathInBundleOrNullForPhotoLibrary]);
+	};
+	
+	/**
+	 * Use this function to set a callback that is called every time the Wikitude SDK encounters an internal error or warning. 
+	 * Internal errors can occur at any time and might not be related to any WikitudePlugin function invocation.
+	 * An error code and message are passed in form of a JSON object.
+	 *
+	 *  @param {function(jsonObject)}  errorHandler  function which is called every time the SDK encounters an internal error.
+	 *
+	 * NOTE: The errorHandler is currently only called by the Wikitude iOS SDK!
+	 */
+	WikitudePlugin.prototype.setErrorHandler = function(errorHandler)
+	{
+		cordova.exec(this.onWikitudeOK, errorHandler, "WikitudePlugin", "setErrorHandler", []);
+	}
+
+	/*
+	 *	=============================================================================================================================
+	 *
+	 *	Callbacks of public functions
+	 *
+	 *	=============================================================================================================================
+	 */
+
+
+	/* Lifecycle updates */
+	/**
+	 *	This function gets called every time the application did become active.
+	 */
+	WikitudePlugin.prototype.onResume = function() {
+
+		// Call the Wikitude SDK that it should resume.
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "onResume", [""]);
+	};
+
+	/**
+	 *	This function gets called every time the application is about to become inactive.
+	 */
+	WikitudePlugin.prototype.onPause = function() {
+
+		// Call the Wikitude SDK that the application did become inactive
+		cordova.exec(this.onWikitudeOK, this.onWikitudeError, "WikitudePlugin", "onPause", [""]);
+	};
+
+	/**
+	 *	A generic success callback used inside this wrapper.
+	 */
+	WikitudePlugin.prototype.onWikitudeOK = function() {};
+
+	/**
+	 *  A generic error callback used inside this wrapper.
+	 */
+	WikitudePlugin.prototype.onWikitudeError = function() {};
+
+
+
+	/* Export a new WikitudePlugin instance */
+	var wikitudePlugin = new WikitudePlugin();
+	module.exports = wikitudePlugin;
+
+});
